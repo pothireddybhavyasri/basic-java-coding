f97a03c464b039dbb82ca0d27113d11fed3be4a2
Pothireddy Bhavya Sri <pothireddybhavyasri@gmail.com>
2026-02-11 18:20:12 +0530

    Add files via upload

---
+++ arrlength.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class arrlength {
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        int[] arr = new int[15];
+        Arrays.fill(arr, -1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
